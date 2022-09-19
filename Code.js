951c9869e8a6c657ac8e4e76c79f68b32aa37900
Qingxin Yuan <99475546+QYuanQx@users.noreply.github.com>
2022-09-19 15:33:00 -0500

    Update Code.js

--- Code.js
+++ Code.js
@@ -5,6 +5,22 @@ const form = FormApp.openById(formId);
 const qrCodeFileId = "1BCo66b-O-ablSuihrBZU1hpc1Kb-woso"
 const qrCodeBlob = DriveApp.getFileById(qrCodeFileId).getAs('image/jpeg')
 
+// spread sheets
+const studentSSId = "1xpvPnj1l3KI9pCac-usnUunV4u5cUXBguhkELRSiUMc";
+const studentSS = SpreadsheetApp.openById(studentSSId);
+
+// sheets
+const studentSheetName = "Form Responses 1";
+const studentSheet = studentSS.getSheetByName(studentSheetName);
+
+// new student sheet related - col number starts from 1
+const studentEmailColNum = 2;
+const studentNameColNum = 3;
+const studentWechatColNum = 4;
+const studentOtherContactColNum = 5;
+const timeColNum = 6;  //  col number for responses containing student arrival date and time
+const timeColNumConf = 7;
+
 function bind(){
     ScriptApp.newTrigger('myOnFormSubmit')
     .forForm(form)
@@ -34,4 +50,39 @@ function sendConfirmationEmail(receiver){
     attachments: [qrCodeBlob]
   });
 }
-  
+
+
+// Check time entered by each student
+function checkTimeConflict(){
+  let studentTimeCol_1 = _getColData(timeColNum, studentSheet);
+  let studentTimeCol_2 = _getColData(timeColNumConf, studentSheet);
+  let studentEmailCol = _getColData(studentEmailColNum, studentSheet)
+
+  for (let i=0; i<studentSheet.getLastRow(); i++){
+  // check whether the time entered twice is same
+    if (studentTimeCol_1[i] != studentTimeCol_2[i]){
+      Logger.log(`sending email - unvalid input time: ${studentEmailCol[i]}`)
+      sendFailureEmailTimeConflict(studentEmailCol[i]);
+    }
+  }
+}
+
+function sendFailureEmailTimeConflict(receiver){
+  Logger.log(`sending failure email due to time inconsistence to ${receiver}`);
+  MailApp.sendEmail({
+    to: receiver,
+    // multi line string!
+    subject: `RCSSA: 请确定接机时间后提交申请`,
+    name: "RCSSA",
+    body: 
+    `新生你好！
+      很抱歉，您的接机报名没有成功。
+      由于您两次填写的接机时间不一致，请您确定好需要接机时间后，如有需要，请您再次填写并提交报名。
+      
+    祝新学期一帆风顺！
+
+    20222-2023 RCSSA全体成员
+    `,
+    attachments: [qrCodeBlob]
+  });
+}
